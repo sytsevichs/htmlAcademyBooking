fbcc7e14feb3ce2e7ff96a6cd747b92114575d58
Sergey Sytsevich <sytsevichs@ya.ru>
2022-06-14 00:38:25 +0300

    Подключны функции получения случайных значений из диапазона

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const randomInteger = function (min, max) {
+  if (min >= max || min < 0 || max <= 0) {
+    return ('Задан неверный диапазон! Укажите другие числа.');
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const randomFloat = function (min, max, digits) {
+  if (min >= max || min < 0 || max <= 0) {
+    return ('Задан неверный диапазон! Укажите другие числа.');
+  }
+  return (Math.floor(Math.random() * (max - min + 1)) + min).toFixed(digits);
+};
+
+randomInteger(0, 1);
+randomFloat(0,1,2);
