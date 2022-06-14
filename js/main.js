bbeedf581553ad30547dce9c2ac7c99e55ac4080
Sergey Sytsevich <sytsevichs@yandex.ru>
2022-06-14 19:27:26 +0300

    Упрощение функций поиска случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 const randomInteger = function (min, max) {
-  if (min >= max || min < 0 || max <= 0) {
-    return ('Задан неверный диапазон! Укажите другие числа.');
-  }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 const randomFloat = function (min, max, digits = 2) {
-  if (min >= max || min < 0 || max <= 0) {
-    return ('Задан неверный диапазон! Укажите другие числа.');
-  }
-  return (Math.random() * (max - min) + min).toFixed(digits);
+  min *= 100;
+  max *= 100;
+  return (randomInteger(min, max) / 100).toFixed(digits);
 };
 
-randomInteger(0, 1);
-randomFloat(0,1,2);
+randomInteger(0, 10);
+randomFloat(0, 10, 2);
