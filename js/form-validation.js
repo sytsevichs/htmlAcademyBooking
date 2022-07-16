daaa902fbd9ee2fd6ab7dc69f01ba85db87f755e
Sergey Sytsevich <sytsevichs@ya.ru>
2022-07-16 21:57:21 +0300

    Рефаторинг и исправление работы с pristine

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,3 +1,6 @@
+import {
+  getWordEnding
+} from './utils/util.js';
 //подключение слайдера noUiSlider для управление ценой
 //import * as noUiSlider from '../node_modules/nouislider/dist/nouislider.js';
 // Деактивация формы
@@ -34,8 +37,8 @@ const adOfferType = adForm.querySelector('#type');
 const adOfferPrice = adForm.querySelector('#price');
 
 const checkFormData = (type) => {
-  adOfferPrice.setAttribute('min',minOfferPrice[type.value]);
-  adOfferPrice.setAttribute('placeholder',minOfferPrice[type.value]);
+  adOfferPrice.setAttribute('min', minOfferPrice[type.value]);
+  adOfferPrice.setAttribute('placeholder', minOfferPrice[type.value]);
   /*
   console.log(noUiSlider);
   //Пользователь может вписать цену в поле, а может указать её перемещением ползунка слайдера
@@ -55,51 +58,52 @@ const checkFormData = (type) => {
 //Устанавливаем начальное минимальное значение при первой загрузке
 checkFormData(adOfferType);
 //Устанавливаем значения при изменении вида предложения.
-adOfferType.addEventListener('change',() => { checkFormData(adOfferType); } );
+adOfferType.addEventListener('change', () => {
+  checkFormData(adOfferType);
+});
 //Поле «Количество комнат» синхронизировано с полем «Количество мест»
-const adOfferRoomNum = adForm.querySelector('#room_number');
-const adOffercapacity = adForm.querySelector('#capacity');
+const adOfferRoomNumber = adForm.querySelector('#room_number');
+const adOfferСapacity = adForm.querySelector('#capacity');
 //функция для определения связки комнат и количества человек
-const getMaxNumber = (value) => {
-  switch (value) {
-    case 0: return 100;
-    case 100: return 0;
-    default: return value;
-  }
+const MAX_ROOMS_NUMBER = 100;
+const CAPACITY = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
 };
-// Функция установки лимитов на элементы
-const setElementLim = (element, value ) => {
-  let min = 1;
-  let max = getMaxNumber(value);
-  if (min > max) {
-    max = min;
-  }
-  if (max === 100) {
-    min = max;
-  }
-  element.setAttribute('min',min);
-  element.setAttribute('max',max);
-};
-//При изменении значений, меняем лимиты связанных элеменов
-adOfferRoomNum.addEventListener('change',() => { setElementLim(adOffercapacity, adOfferRoomNum.value); } );
-adOffercapacity.addEventListener('change',() => { setElementLim(adOfferRoomNum, adOffercapacity.value); } );
-//Поля «Время заезда» и «Время выезда» синхронизированы
-const adOfferCheckIn = adForm.querySelector('#timein');
-const adOfferCheckOut = adForm.querySelector('#timeout');
-adOfferCheckIn.addEventListener('change',() => { adOfferCheckOut.value = adOfferCheckIn.value; } );
-adOfferCheckOut.addEventListener('change',() => { adOfferCheckIn.value = adOfferCheckOut.value;  } );
-
-//console.log(adOffercapacity.getAttribute('max'));
-const pristine = new Pristine(adForm);
+//Добавляем функцию проверки комнат и постояльцев
+const capacityIsValid = () => CAPACITY[adOfferRoomNumber.value].includes(adOfferСapacity.value);
+const capacityErrorMessage = () => `Ошибка: ${(adOfferСapacity.value < MAX_ROOMS_NUMBER) && (adOfferСapacity.value !== '0') ?
+  `${adOfferRoomNumber.value} комнат${getWordEnding(adOfferRoomNumber.value, false)} не вмещает ${adOfferСapacity.value} гост${getWordEnding(adOfferRoomNumber.value, true)}` :
+  `${MAX_ROOMS_NUMBER} комнат не для проживания гостей!`}`;
+//Проверки сторонней библиотекой Pristine
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__item--invalid',
+  successClass: 'ad-form__item--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__error'
+});
+//Добавляем пользовательскую проверку предложения
+pristine.addValidator(adOfferСapacity, capacityIsValid, capacityErrorMessage);
 
 adForm.addEventListener('submit', (evt) => {
   const isValid = pristine.validate();
-  if (!isValid || (adOfferRoomNum.value === 100 & adOffercapacity.value>0)
-               || (adOffercapacity.value === 0 & adOfferRoomNum.value<100)
-               || (adOffercapacity.value !== adOfferRoomNum.value & adOfferRoomNum.value !== 100))
-  {
-    return evt.preventDefault();
+  if (!isValid) {
+    evt.preventDefault();
   }
+});
+
+//Поля «Время заезда» и «Время выезда» синхронизированы
+const adOfferCheckIn = adForm.querySelector('#timein');
+const adOfferCheckOut = adForm.querySelector('#timeout');
+adOfferCheckIn.addEventListener('change', () => {
+  adOfferCheckOut.value = adOfferCheckIn.value;
+});
+adOfferCheckOut.addEventListener('change', () => {
+  adOfferCheckIn.value = adOfferCheckOut.value;
 });
 
 /**/
